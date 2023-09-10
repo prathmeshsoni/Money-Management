7e40f2830983fb9bd031e21d1a8850b55d46d5a8
prathmesh <prathmesh.soni51@gmail.com>
2023-09-10 16:58:54 +0530

    Update at 07-09-2023

--- wallet/settings.py
+++ wallet/settings.py
@@ -84,25 +84,25 @@ WSGI_APPLICATION = 'wallet.wsgi.application'
 """
     Mysql Database
 """
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_NAME',
-        'HOST': 'localhost',
-        'USER': 'db_USER',
-        'PASSWORD': 'db_PASSWORD',
-    },
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'wallet',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'password',
+#     },
+# }
 
 """
     Sqlite Database
 """
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
